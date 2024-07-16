7c35840adb58e214750f27c506b9d243c7f436b2
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-07-16 13:02:11 -0400

    - Added message ID to model

--- simplebackend/models.py
+++ simplebackend/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class Message(models.Model):
+    messageid = models.IntegerField(primary_key=True)
     message = models.CharField(max_length=100)
     date_created = models.DateTimeField(auto_now_add=True)
