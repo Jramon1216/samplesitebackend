8384f2f2a195826168744765dc0f58c0de862c4f
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-07-16 13:05:58 -0400

    - Added a null values to all fields

--- simplebackend/models.py
+++ simplebackend/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Message(models.Model):
-    messageid = models.IntegerField(primary_key=True)
-    message = models.CharField(max_length=100)
-    date_created = models.DateTimeField(auto_now_add=True)
+    messageid = models.IntegerField(primary_key=True, null=False)
+    message = models.CharField(max_length=100, null=False)
+    date_created = models.DateTimeField(auto_now_add=True, null=False)
