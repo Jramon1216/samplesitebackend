38a3797e563ab19a69ffb0f5646c6f289d6db409
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-07-16 13:41:54 -0400

    - made migrations

--- samplesitebackend/urls.py
+++ samplesitebackend/urls.py
@@ -1,11 +1,3 @@
-"""
-URL configuration for samplesitebackend project.
-
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-"""
-
 from django.contrib import admin
 from django.urls import path
 from simplebackend import views
--- simplebackend/views.py
+++ simplebackend/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from.models import Message
+from .models import Message
 
 def receive_message(request):
-    return HttpResponse("this endpoint works!")
+    msg = Message.objects.first()
+    return HttpResponse(msg)
 
 def send_message(request):
     return HttpResponse("This endpoint also works!")
