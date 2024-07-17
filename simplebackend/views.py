919e779df57a46903574873000236f9de086fef7
Jonathan Ramon <35818830+Jramon1216@users.noreply.github.com>
2024-07-17 13:55:25 -0400

    - Added comments

--- simplebackend/views.py
+++ simplebackend/views.py
@@ -12,31 +12,31 @@ import random
 @csrf_exempt
 @api_view(['GET'])
 def receive_message(request):
-    if request.method == 'GET':
+    if request.method == 'GET': 
         try:
-            messages = Message.objects.values('message')
-            if messages.exists():
-                msg_list = [m['message'] for m in messages]
+            messages = Message.objects.values('message') # Get all messages (specifically the message value from the db)
+            if messages.exists(): 
+                msg_list = [m['message'] for m in messages] 
                 length = len(msg_list)
-                random_message = msg_list[random.randint(0,length-1)]
-                return Response({'RECEIVED MESSAGE': f'{random_message}'}, status=status.HTTP_200_OK) 
+                random_message = msg_list[random.randint(0,length-1)] # Get a random message from the list of messages
+                return Response({'RECEIVED MESSAGE': f'{random_message}'}, status=status.HTTP_200_OK)  # Return message response
             else:
-                return Response({"EMPTY": "There are no messages to receive"}, status=status.HTTP_204_NO_CONTENT)
+                return Response({"EMPTY": "There are no messages to receive"}, status=status.HTTP_204_NO_CONTENT) 
         except Exception as e:
-            return Response({'ERROR': f'Something went wrong! Please Try again later - {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
-    return Response({'ERROR': 'Invalid HTTP method'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return Response({'ERROR': f'Something went wrong! Please Try again later - {str(e)}'}, status=status.HTTP_400_BAD_REQUEST) # Something is wrong, raise exception
+    return Response({'ERROR': 'Invalid HTTP method'}, status=status.HTTP_405_METHOD_NOT_ALLOWED) 
 
 @csrf_exempt
 @api_view(['POST'])
 def send_message(request):
     if request.method == 'POST':
         try:
-            serializer = MessageSerializer(data=request.data)
+            serializer = MessageSerializer(data=request.data) # Serialize data (parse data)
             if serializer.is_valid():
-                serializer.save()
-                return Response({'SUCCESS':'Message sent!'}, status=status.HTTP_201_CREATED)
+                serializer.save() # Save record into DB
+                return Response({'SUCCESS':'Message sent!'}, status=status.HTTP_201_CREATED) # Success message
             else:
-                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # Bad data, respond with serialzier error
         except Exception as e:
-            return Response({'ERROR': f'Something went wrong! Please Try again later - {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'ERROR': f'Something went wrong! Please Try again later - {str(e)}'}, status=status.HTTP_400_BAD_REQUEST) # Something is wrong, raise exception
     return Response({'ERROR': 'Invalid HTTP method'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
